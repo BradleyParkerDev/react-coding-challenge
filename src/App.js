96aabd5b208478c0a9c0546d8cb84b59ccf4dd4b
Bradley Parker <Bradleyscottparker@outlook.com>
2023-05-27 19:22:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import Star from './Components/Star';
+import { useState } from 'react';
 
-function App() {
+function App(props) {
+  
+  //Count initial state is 0
+  const [count,setCount] = useState(0);
+
+  //Add one to count
+  const add = () => {
+    
+    if(count < 5){
+        setCount(count + 1);
+        console.log(count)
+    }
+  }
+
+  //Remove one from count
+  const remove = () => {  
+    if(count <= 5 && count > 0){
+        setCount(count - 1);
+        console.log(count)
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="outer-container">
+      <div id="inner-container">
+        <div className="inner-divs" style={{backgroundColor: "red", justifyContent: "space-evenly"}}>
+          <p style={{fontWeight: "bold", marginRight: "60%", marginTop: "10%"}}>LOGO</p>
+          <p style={{marginTop: "10%"}}>menu</p>
+        </div>
+        <div className="inner-divs" style={{justifyContent: "space-evenly"}}>
+          <button 
+          className="buttons"
+          onClick ={remove}
+          >
+              Remove 1
+          </button>
+          <h3>{count}</h3>
+          <button 
+          className="buttons"
+          onClick={add}
+          >
+            Add 1
+          </button>
+        </div>
+        <div className="inner-divs">
+          <Star count = {props.count} />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
