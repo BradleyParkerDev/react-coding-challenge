44b75df68985b790d40228464f3390d426590bb7
Bradley Parker <Bradleyscottparker@outlook.com>
2023-05-28 01:19:27 -0500

    assignment completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import Star from './Components/Star';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App(props) {
   
   //Count initial state is 0
   const [count,setCount] = useState(0);
+  const [starsArr, setStarsArr] = useState([])
 
   //Add one to count
   const add = () => {
     
     if(count < 5){
         setCount(count + 1);
-        console.log(count)
     }
   }
 
@@ -20,11 +20,34 @@ function App(props) {
   const remove = () => {  
     if(count <= 5 && count > 0){
         setCount(count - 1);
-        console.log(count)
     }
   }
 
-
+  //useEffect watches count to shorten or lengthen starsArr
+  useEffect((props)=>{
+    console.log(`Count is: ${count}`)
+    switch(count){
+      case 0:
+        setStarsArr([]);
+        break;     
+      case 1:
+        setStarsArr([1]);
+        break;
+      case 2:
+        setStarsArr([1,2]);
+        break;
+      case 3:
+        setStarsArr([1,2,3]);
+        break;
+      case 4:
+        setStarsArr([1,2,3,4]);
+        break;
+      case 5:
+        setStarsArr([1,2,3,4,5]);
+        break;          
+    }
+  },[count])
+  
   return (
     <div id="outer-container">
       <div id="inner-container">
@@ -48,7 +71,9 @@ function App(props) {
           </button>
         </div>
         <div className="inner-divs">
-          <Star count = {props.count} />
+          {
+            starsArr.map( star =>(<Star/>))
+          }
         </div>
       </div>
     </div>
--- src/Components/Star.js
+++ src/Components/Star.js
@@ -1,24 +1,11 @@
 import '../App.css';
 import star from './star.png'
-import { useState, useEffect } from 'react';
 
 const Star = (props) => {
-    const { count } = props;
-    
-    const imgPath = "/Users/bparkerpro/Desktop/Code-Immersives-Workspace/300-workspace/301/react-coding-challenge/public/star.png"
-    useEffect((props) =>{
-
-
-        console.log(`Count: ${count}`)
-
-    },[count])
-
-
-
     return(
         <div className='stars'>
             <img id="star-image" src={star}></img>
-        </div>
+        </div>                
     );
 }
 
